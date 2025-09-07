f548e46080c75df931a24db43e02a5cd69a7dc98
gromax <gromax212live@gmail.com>
2025-09-07 21:13:57 +0530

    dasd

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-from flask import Flask
+from flask import Flask,redirect,jsonify
 
 app = Flask(__name__)
 
+@app.route("/bad_request")
+def bad_request():
+    return jsonify(error="Something went wrong"), 400
+
+@app.route("/denied")
+def not_found():
+    return jsonify(error="Access Denied"), 403
+
+@app.route("/server_error")
+def server_error():
+    return jsonify(error="Internal server error"), 500
 
 @app.route("/")
 def home():
     with open("res.txt","r") as f:
         ad = f.read()
-        return ad
+        return redirect("https://"+ad)
 
-@app.route("/change/<ip>")
-def change(ip):
+@app.route("/change/<ip>/<passs>")
+def change(ip,passs):
+    if passs != "yessir":
+        return redirect("/denied")
     with open("res.txt","w") as f:
         f.write(ip)
-    return "ok"
+    return "ok",200
 
 @app.route("/logs", methods=["GET", "POST"])
-def home():
+def logs():
     if request.method == "POST":
         data = request.get_json()
         name = data.get("name")
