f06b06e14f8b9027bf5abbfb7962d8ac1a4d42f6
gromax <gromax212live@gmail.com>
2025-10-01 22:19:13 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from flask import Flask,redirect,jsonify
 import requests
+import psycopg2
+
+DATABASE_URL = "postgresql://data_store_7lzv_user:0xZ0gSg6Lp6bcG15g9XkRFL1mjZLT161@dpg-d3ekj2r3fgac7389ua2g-a.oregon-postgres.render.com:5432/data_store_7lzv"
+
+
+conn = psycopg2.connect(DATABASE_URL)
+cur = conn.cursor()
+
+
 app = Flask(__name__)
 
 @app.route("/bad_request")
@@ -16,20 +25,26 @@ def server_error():
 
 @app.route("/")
 def home():
-    with open("res.txt","r") as f:
-        ad = f.read()
-        pig = requests.get(f"https://"+ad).text
-        if "offline" in pig:
-            return "Server is Offline!"
-        else:
-            return redirect("https://"+ad)
+    cur.execute("SELECT url FROM ngrok_url;")
+    row = cur.fetchone()[0]
+    check_ping = requests.get(row)
+    if check_ping.status_code == 200:
+        return redirect(row)
+    else:
+        reg = check_ping.headers
+        cur.execute("SELECT updated_at FROM ngrok_url;")
+        up = cur.fetchone()[0]
+        return f"Server is offline or wrong url \n ping result:\n {reg} \n last_update={up}"
+
 
 @app.route("/change/<ip>/<passs>")
 def change(ip,passs):
     if passs != "yessir":
         return redirect("/denied")
-    with open("res.txt","w") as f:
-        f.write(ip)
+
+    cur.execute(f"update ngrok_url SET url='https://{ip}' , updated_at=NOW() where id=1;")
+    conn.commit()
+
     return "ok",200
 
 @app.route("/logs", methods=["GET", "POST"])
